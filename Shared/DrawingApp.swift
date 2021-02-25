c9c2783ad99a8c623968f530c6948d69520dfb0e
Jacob LeCoq <lecoqjacob@gmail.com>
2021-02-25 14:27:15 -0600

    init

---
+++ Shared/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Shared
+//
+//  Created by Jacob LeCoq on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
